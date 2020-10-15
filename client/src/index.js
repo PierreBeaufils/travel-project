7a8a99ade4c5022efae68599f224cea58a73ea3f
LeMellow <pierre.beaufils@outlook.fr>
2020-10-15 16:54:08 +0200

    install axios & react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,11 @@
-// == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-// == Import : local
-// Composants
 import App from 'src/components/App';
-// Store
 import store from 'src/store';
 
-// == Render
-// 1. Élément React racine (celui qui contient l'ensemble de l'app)
-//    => crée une structure d'objets imbriqués (DOM virtuel)
+
 const rootReactElement = (
   <Provider store={store}>
     <App />
