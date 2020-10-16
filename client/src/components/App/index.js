69ee5e77414d8c4cf7fd513b33bf8a531e73a4dc
LeMellow <pierre.beaufils@outlook.fr>
2020-10-16 15:50:52 +0200

    mentions legales pages

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './styles.css';
 
 import Navbar from 'src/components/Navbar';
-import Homepage from 'src/components/Homepage';
 import Footer from 'src/components/Footer';
-
+import Homepage from 'src/components/Homepage';
+import Mentions from 'src/components/Mentions';
+/*
+import Team from 'src/components/Team';
+import Contact from 'src/components/Contact';
+import About from 'src/components/About';
+*/
 // == Composant
 const App = () => (
   <div className="app">
     <Navbar />
-    <Homepage />
+    <Switch>
+      <Route path="/" exact component={Homepage} />
+      <Route path="/mentions-legales" component={Mentions} />
+      {/*
+    <Route
+      path="/equipe"
+      component={Team}
+    />
+    <Route
+      path="/contact"
+      component={Contact}
+    />
+    <Route
+      path="/a-propos"
+      component={About}
+    />
+    */}
+    </Switch>
     <Footer />
   </div>
 );
