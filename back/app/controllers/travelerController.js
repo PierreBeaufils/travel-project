c1c6bac36775138e4d538c12c6ce8ccd1f53d1cd
Limsa00 <bennis.salim@gmail.com>
2020-10-16 12:02:33 +0200

    routes defined

---
+++ back/app/controllers/travelerController.js
@@ -0,0 +1,37 @@
+const Traveler = require('../models/Traveler')
+
+const travelerController = {
+    loginForm: (req, res) => {
+        
+    },
+
+    doLogin: async (req, res) => {
+
+    },
+    
+    signupForm: (req, res) => {
+
+    },
+
+    doSignup: async (req, res) => {
+
+    },
+
+    logout: (req, res) => {
+
+    },
+
+    profile: (req, res) => {
+        
+    },
+
+    modifyProfile: (req, res) => {
+
+    },
+
+    doContact: (req, res) => {
+
+    }
+};
+
+module.exports = travelerController
