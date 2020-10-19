c2a01bf205a51b3dd83d1bc8285d12357fb97c6a
Limsa00 <bennis.salim@gmail.com>
2020-10-19 18:08:51 +0200

    dologin ok // signup to finish & bcrypt to implement

--- back/app/controllers/loginController.js
+++ back/app/controllers/loginController.js
@@ -1,10 +1,33 @@
+const Traveler = require('../models/Traveler');
+const bcrypt = require ('bcrypt');
+
 const loginController = {
     loginForm: async(req, res) => {
 
     },
 
     doLogin: async (req, res) => {
+        const user = await Traveler.findByEmail(req.body.email)
+        if (!user){
+            res.json('utilisateur introuvable');
+        } else {
+            if (req.body.password !== user.password){
+                res.json('le mot de passe est incorrect');                
+            } else {
+                req.session.user = {
+                    firstname : user.first_name,
+                    lastname : user.last_name,
+                    email: user.email,
+                    role: user.role
+                }
 
+                if (req.body.remember) {
+                    req.cookie.expires = new Date (Date.now() + 60*60*24)
+                }
+                res.json(user);
+                res.redirect('/')
+            }            
+        }
     },
     
     signupForm: (req, res) => {
@@ -12,6 +35,12 @@ const loginController = {
     },
 
     doSignup: async (req, res) => {
+        const user = await Traveler.findByEmail(req.body.email)
+        if (user) {
+            res.json ('cet adresse email existe déjà');
+        } else {
+            
+        }
 
     },
 
