ea739f2367106e2f8e056f0b1ceac31851ce06cf
Bruno973 <sbad973@gmail.com>
2020-10-19 10:23:47 +0200

    ajout des models activity et transport qui hérite de coremodels

--- back/app/controllers/mainController.js
+++ back/app/controllers/mainController.js
@@ -1,9 +1,19 @@
 const accommodation = require("../models/Accomodation");
+const activity = require("../models/Activity");
+const transport = require("../models/Transport");
 
 
 const mainController = {
     test: async (req, res) =>{
         let travelAccomodations = await accommodation.findAllTravelComponent(4);
+        res.json(travelAccomodations);
+    },
+    test2: async (req, res) =>{
+        let travelAccomodations = await activity.findAllTravelComponent(6);
+        res.json(travelAccomodations);
+    },
+    test3: async (req, res) =>{
+        let travelAccomodations = await transport.findAllTravelComponent(4);
         res.json(travelAccomodations);
     }
 };
