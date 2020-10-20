1c0da086a318d97716d9530841a15a56a3a4c67f
LeMellow <pierre.beaufils@outlook.fr>
2020-10-20 10:44:37 +0200

    handlelogin proptypes definition

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-const LoginForm = () => {
+const LoginForm = ({ email, password, changeField, handleLogin }) => {
   const {
-    register, handleSubmit, watch, errors,
+    register, errors,
   } = useForm();
-  const onSubmit = (data) => console.log(data);
 
-  console.log(watch('example')); // watch input value by passing the name of it
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleLogin();
+  };
 
   return (
     <div className="login">
       <div className="main-form">
         <h2>Connexion</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email
-            <input name="email" ref={register({ required: true })} type="email" />
+            <input name="email" ref={register({ required: true })} type="email" value={email} onChange={changeField} />
             {errors.email && <span className="warning-text">Veuillez saisir votre email</span>}
           </label>
           <label htmlFor="password">Mot de passe
-            <input name="password" ref={register({ required: true })} type="password" />
+            <input name="password" ref={register({ required: true })} type="password" value={password} onChange={changeField} />
             {errors.password && <span className="warning-text">Veuillez saisir un mot de passe valide</span>}
           </label>
 
@@ -32,12 +34,12 @@ const LoginForm = () => {
     </div>
   );
 };
-// LoginForm.propTypes = {
 
-// };
-
-// LoginForm.defaultProps = {
-
-// };
+LoginForm.propTypes = {
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  changeField: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+};
 
 export default LoginForm;
