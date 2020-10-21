845efd3ca65149c8e12bd8cee53adcf4331eaa80
Bruno973 <sbad973@gmail.com>
2020-10-21 14:03:45 +0200

    ajout des CORS policy

--- back/app/server.js
+++ back/app/server.js
@@ -18,6 +18,15 @@ const port = process.env.PORT || 5555;
 
 const router = require('./router');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
+next();
+};
+  
+
 app.use(express.json());
 app.use('/v1', router);
 
