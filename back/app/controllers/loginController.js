79d774f38cb02f5f01dcab5450a4c02c0c666882
Limsa00 <bennis.salim@gmail.com>
2020-10-22 10:36:28 +0200

    loginController status update

--- back/app/controllers/loginController.js
+++ back/app/controllers/loginController.js
@@ -5,13 +5,15 @@ const loginController = {
     doLogin: async (req, res) => {
         const user = await Traveler.findByEmail(req.body.email)
         if (!user){
-            res.json('utilisateur introuvable');
+            res.status(401).json('utilisateur introuvable');
+            // res.json('utilisateur introuvable');
         } else {
 
             const validPwd = bcrypt.compareSync(req.body.password, user.password); 
             
             if (!validPwd){
-                res.json('le mot de passe est incorrect');                
+                res.status(401).json('le mot de passe est incorrect');
+                // res.json('le mot de passe est incorrect');                
             } else {
                 req.session.user = {
                     firstname : user.first_name,
@@ -19,10 +21,12 @@ const loginController = {
                     email: user.email,
                     role: user.role
                 };
+                
+                req.session.cookie.expires = new Date (Date.now() + 60*60*24);
 
-                if (req.body.remember) {
-                    req.cookie.expires = new Date (Date.now() + 60*60*24)
-                };
+                // if (req.body.remember) {
+                //     req.cookie.expires = new Date (Date.now() + 60*60*24)
+                // };
                 
                 res.json(user);
                 // res.redirect('/')
@@ -33,10 +37,11 @@ const loginController = {
     doSignup: async (req, res) => {
         const user = await Traveler.findByEmail(req.body.email)
         if (user) {
-            res.json ('cette adresse email existe déjà');
+            res.status(403).json('cette adresse email existe déjà');
+            // res.json ('cette adresse email existe déjà');
         } else {
             if (req.body.password !== req.body.passwordConfirm) {
-                res.json('la confirmation du mot de passe est incorrecte');
+                res.status(403).json('la confirmation du mot de passe est incorrecte');
             } else {
                 const hashPwd = bcrypt.hashSync(req.body.password, 10);
 
@@ -56,6 +61,7 @@ const loginController = {
 
     logout: (req, res) => {
         req.session.user = false;
+        res.json('tu es maintenant déconnecté');
         res.redirect('/');
     },
 };
