55e8a72a837f75676768a7489fa49fb3df1c89e8
Limsa00 <bennis.salim@gmail.com>
2020-10-23 12:09:20 +0200

    task routes OK + route get travels + cleaning of router.js

--- back/app/controllers/mainController.js
+++ back/app/controllers/mainController.js
@@ -1,29 +1,171 @@
-const accommodation = require("../models/Accommodation");
-const activity = require("../models/Activity");
-const transport = require("../models/Transport");
-const traveler = require("../models/Traveler");
-const task = require('../models/Task');
+const Accommodation = require("../models/Accommodation");
+const Activity = require("../models/Activity");
+const Transport = require("../models/Transport");
+const Travel = require("../models/Travel");
+const Task = require ("../models/Task");
+
+const objectModel = [Accommodation,Activity,Transport,Travel, Task];
 
 
-const mainController = {
-    test: async (req, res) =>{
+const travelController = {
+    createTravel: async (req,res) => {
+        const newTravel = new Travel(req.body);
+        const savedTravel = await newTravel.saveAllTravelComponent();
+        res.json(savedTravel);
+        // Theme / destination / date départ / date de fin
+    },
+    showAllInfos: async (req,res) => {
+        const travelId = req.params.id;
+        const travelinfos = {};
+        travelinfos.infos = await Travel.findOneTravelComponent(null,travelId);
+        travelinfos.accommodation = await Accommodation.findAllTravelComponent(travelId);
+        travelinfos.activity = await Activity.findAllTravelComponent(travelId);
+        travelinfos.transport = await Transport.findAllTravelComponent(travelId);
+        res.json(travelinfos);
+    },
 
+    editTravel: async (req,res) => {
+        const travelToEdit = await travel.findOneTravelComponent(null,req.params.id);
+        const travelEdited = await new Travel(travelToEdit);
+        travelEdited.update(req.body);
+        res.json(await travelEdited.saveAllTravelComponent());
+    },
+    showTravels: async (req, res) =>{
+        const allTravels = await Travel.findAllTravelComponent();
         
-       const task = await Task.findOne(req.param.id);
-        const taskToSave = new Task(task);
-        accommodationToSave.saveAllTravelComponent(req.params.id);
+        if (allTravels.length > 0) { 
+            res.json(allTravels);
+        } else {
+            res.json ('Cet hébergement n\'existe pas');
+        };
+    },
+    showAccommodations: async (req, res) =>{
+        const travelId = req.params.id ;
+        const travelAccommodations = await Accommodation.findAllTravelComponent(travelId);
+        
+        if (travelAccommodations.length > 0) { 
+            res.json(travelAccommodations);
+        } else {
+            res.json ('Cet hébergement n\'existe pas');
+        };
+    },
+    showTransport: async (req, res) =>{ 
+        const travelId = req.params.id ;
+        const travelTransport = await Transport.findAllTravelComponent(travelId);
 
-        // let travelAccommodations = await accommodation.findAllTravelComponent(4);
-        // res.json(travelAccommodations);
+        if (travelTransport.length > 0) { 
+            res.json(travelTransport);
+        } else {
+            res.json ('Ce transport n\'existe pas');
+        };
     },
-    test2: async (req, res) =>{
-        let travelAccommodations = await activity.findAllTravelComponent(6);
-        res.json(travelAccommodations);
+    showActivity: async (req, res) =>{
+        const travelId = req.params.id ;
+        const travelActivity = await Activity.findAllTravelComponent(travelId);
+
+        if (travelActivity.length > 0) { 
+            res.json(travelActivity);
+        } else {
+            res.json ('Cette activité n\'existe pas');
+        };
     },
-    test3: async (req, res) =>{
-        let travelAccommodations = await transport.findAllTravelComponent(4);
-        res.json(travelAccommodations);
+    showTask: async (req, res) =>{
+        const travelId = req.params.id ;
+        const travelTask = await Task.findAllTravelComponent(travelId);
+
+        if (travelTask.length > 0) { 
+            res.json(travelTask);
+        } else {
+            res.json ('Cette tâche n\'existe pas');
+        };
+    },
+    showUserTravels : async (req,res) =>{
+        const travelerId = req.params.id;
+        const userTravels = await Travel.findAllTravels(travelerId);
+        res.json(userTravels);
+    },
+    createAccommodation: async (req,res) => {
+        const newAcco = new Accommodation(req.body);
+        const savedAcco = await newAcco.saveAllTravelComponent();
+        res.json("Ajout effectué");
+    },
+    createActivity: async (req,res) => {
+        const newActiv = new Activity(req.body);
+        const savedActiv = await newActiv.saveAllTravelComponent();
+        res.json("Ajout effectué");
+    },
+    createTransport: async (req,res) => {
+        const newTransp = new Transport(req.body);
+        const savedTransp = await newTransp.saveAllTravelComponent();
+        res.json("Ajout effectué");
+    },
+    createTask: async (req,res) => {
+        const newTask = new Task(req.body);
+        const savedTask = await newTask.saveAllTravelComponent();
+        res.json("Ajout effectué");
+    },
+    editAccommodation: async (req,res) => {
+        const accoToEdit = await Accommodation.findOneTravelComponent(req.params.id,req.params.accoId);
+        const accoEdited = await new Accommodation(accoToEdit);
+        accoEdited.update(req.body);
+        accoEdited.saveAllTravelComponent();
+        res.json("Hébergement mis à jour");
+    },
+    editTransport: async (req,res) => {
+        const transportToEdit = await Transport.findOneTravelComponent(req.params.id,req.params.transportId);
+        const transportEdited = await new Transport(transportToEdit);
+        transportEdited.update(req.body);
+        transportEdited.saveAllTravelComponent();
+        res.json("Transport mis à jour"); 
+    },
+    editActivity: async (req,res) => {
+        const activityToEdit = await Activity.findOneTravelComponent(req.params.id,req.params.activityId);
+        const activityEdited = await new Activity(activityToEdit);
+        activityEdited.update(req.body);
+        activityEdited.saveAllTravelComponent();
+        res.json("Activité mise à jour"); 
+    },
+    editTask: async (req,res) => {
+        const taskToEdit = await Task.findOneTravelComponent(req.params.id,req.params.taskId);
+        const taskEdited = await new Task(taskToEdit);
+        taskEdited.update(req.body);
+        taskEdited.saveAllTravelComponent();
+        res.json("Task mise à jour"); 
+    },
+    delete: async (req,res) => {
+        const travelToFind = await Travel.findOneTravelComponent(null,req.params.id);
+        const travelToDelete = new Travel(travelToFind);
+        await travelToDelete.delete();
+        res.json("suppresion effectuée");
+    },
+    deleteEntity: async (req,res) => {
+        let entity = req.params.entity;
+        // entity = entity.charAt(0).toUpperCase() + entity.slice(1);
+        let entityToUse ;
+        
+        for (let i = 0 ; i < objectModel.length ; i++) {
+            // console.log('objectModel[i].tableName', objectModel[i].tableName);
+            if (entity === objectModel[i].tableName) {
+                entityToUse = objectModel[i];
+            }
+        };
+
+        // console.log('entityToUse: ', entityToUse);
+        // console.log('entityToUse.tableName: ', entityToUse.tableName);
+        // console.log('req.params.id: ',req.params.id)
+        // console.log('req.params.entityId: ',req.params.entityId)
+        
+        const entityToFind = await entityToUse.findOneTravelComponent(req.params.id,req.params.entityId);
+        // console.log("entityToFind: ", entityToFind);
+        if (entityToFind) {
+            const entityToDelete = new entityToUse(entityToFind);
+            await entityToDelete.delete();
+            res.json("suppresion effectuée");
+        } else {
+            res.json("cette entité n'existe pas");
+        }
+
     }
 };
 
-module.exports = mainController ;
+module.exports = travelController ;
--- back/app/router.js
+++ back/app/router.js
@@ -1,93 +1,77 @@
 const mainController = require('./controllers/mainController');
 const travelerController = require('./controllers/travelerController');
-const adminController = require('./controllers/adminController');
-const travelController = require('./controllers/travelController');
 const loginController = require('./controllers/loginController');
 
 const { Router } = require('express');
 
 const router = Router();
 
-// router.post('/',mainController.test);
-
-// router.patch('/',mainController.test);
-// router.get('/test1',mainController.test3);
-// router.get('/test2',mainController.test2);
-
+// INFOS VOYAGEURS : affichage globale et individuel, insertion, modification et suppression
 router.get('/travelers', travelerController.allTravelers);
 router.get('/traveler/:id', travelerController.getOneTraveler);
 router.post('/traveler', travelerController.newTraveler);
 router.patch('/traveler/:id', travelerController.editTraveler);
 router.delete('/traveler/:id', travelerController.deleteTraveler);
+// VOYAGES LIES A UN VOYAGEUR : Voyages à venir auxquels un voyageur est inscrit
+router.get('/user-travels/:id', mainController.showUserTravels);
 
-router.get('/user-travels/:id', travelController.showUserTravels);
+// INFOS DU VOYAGE : affichage globale et individuel, insertion, modification et suppression
+router.get('/travels', mainController.showTravels);
+router.get('/travel/:id', mainController.showAllInfos);
+router.post('/create-travel', mainController.createTravel);
+router.patch('/travel/:id', mainController.editTravel);
+router.delete('/travel/:id', mainController.delete);
 
+// INFOS HEBERGEMENTS LIES A UN VOYAGE : affichage globale et individuel, insertion, modification et suppression 
+router.get('/travel/:id/accommodation', mainController.showAccommodations);
+router.post('/travel/:id/accommodation', mainController.createAccommodation);
+router.patch('/travel/:id/accommodation/:accoId', mainController.editAccommodation);
 
-// Formulaire de login : affichage et traitement
-router.post('/login', loginController.doLogin);
+// INFOS ACTIVITES LIEES A UN VOYAGE : affichage globale et individuel, insertion, modification et suppression 
+router.get('/travel/:id/activity', mainController.showActivity);
+router.post('/travel/:id/activity', mainController.createActivity);
+router.patch('/travel/:id/activity/:activityId', mainController.editActivity);
 
-router.post('/isLogged', loginController.loginCheck);
-// Formulaire de signup : affichage et traitement
-router.post('/signup', loginController.doSignup);
-// Déconnexion
-router.post('/logout', loginController.logout);
-// Infos persos de l'utilisateur : affichage et traitement
-// router.get('/profile', travelerController.profile);
-// router.post('/profile', travelerController.modifyProfile);
-// page de contact (mail prérempli)
+// INFOS TRANSPORT LIES A UN VOYAGE : affichage globale et individuel, insertion, modification et suppression
+router.get('/travel/:id/transport', mainController.showTransport);
+router.post('/travel/:id/transport', mainController.createTransport);
+router.patch('/travel/:id/transport/:transportId', mainController.editTransport);
 
+// INFOS TACHES LIES A UN VOYAGE : affichage globale et individuel, insertion, modification et suppression 
+router.get('/travel/:id/task', mainController.showTask);
+router.post('/travel/:id/task', mainController.createTask);
+router.patch('/travel/:id/task/:taskId', mainController.editTask);
 
-// router.get('/travel/id/travelers', travelerController.allTravelers);
-// Voir tout les travelers associé à un voyage
+// ROUTE FACTORISEE POUR SUPPRESSION de Hébergement, activité, transport, task liés à un voyage
+router.delete('/travel/:id/:entity/:entityId', mainController.deleteEntity);
 
-//cette route sera protégée par le middleware adminMW
-//Seuls les utilisateurs avec un rôle admin pourront y avoir accès
-// router.get('/admin', adminMW, adminController.admin);
-
-// Page dashboard
-// router.get('/dashboard', Controller)
-
-// Créer un voyage
-router.post('/create-travel', travelController.createTravel);
-
-router.get('/travel/:id', travelController.showAllInfos) //Toutes les datas du voyage Voyageurs + Acco + Transp + activities 
-// router.post('/travel/:id', travelController) => Elle fait quoi ???
-router.patch('/travel/:id', travelController.editTravel);
-router.delete('/travel/:id', travelController.delete);
-
-// infos liées à l'hébergement d'un voyage : affichage, insertion, modification suppression 
-router.get('/travel/:id/accommodation', travelController.showAccommodations);
-router.post('/travel/:id/accommodation', travelController.createAccommodation);
-router.patch('/travel/:id/accommodation/:accoId', travelController.editAccommodation);
-// router.delete('/travel/:id/accommodation', travelController)
-
-// infos liées aux activités d'un voyage : affichage, insertion, modification suppression 
-router.get('/travel/:id/activity', travelController.showActivity);
-router.post('/travel/:id/activity', travelController.createActivity);
-router.patch('/travel/:id/activity/:activityId', travelController.editActivity);
-// router.delete('/travel/:id/activity', travelController)
-
-// Info liés aux transport d'un voyage :
-router.get('/travel/:id/transport', travelController.showTransport);
-router.post('/travel/:id/transport', travelController.createTransport);
-router.patch('/travel/:id/transport/:transportId', travelController.editTransport);
-
-router.delete('/travel/:id/:entity/:entityId', travelController.deleteEntity);
-
-// infos liées aux tâches d'un voyage : affichage, insertion, modification suppression 
-// router.get('/travel/:id/task', travelController)
-// router.post('/travel/:id/task', travelController)
-// router.patch('/travel:id/task/:id', mainController.test);
-// router.delete('/travel/:id/task', travelController)
-
-// infos liées aux documents d'un voyage : affichage, insertion, modification suppression 
+// INFOS DOCUMENTS LIES A UN VOYAGE : affichage globale et individuel, insertion, modification et suppression
 // router.post('/travel/:id/document', travelController)
 // router.patch('/travel/:id/document', travelController)
 // router.delete('/travel/:id/document', travelController)
+
+// Formulaire de login : traitement
+router.post('/login', loginController.doLogin);
+// MW de vérification de session de login
+router.post('/isLogged', loginController.loginCheck);
+// Formulaire de signup : traitement
+router.post('/signup', loginController.doSignup);
+// Déconnexion
+router.post('/logout', loginController.logout);
 
 // Routes optionnelles
 // '/travel/:id/activity/:id'
 // '/travel/:id/transport/:id'
 // '/travel/:id/accommodation/:id'
+
+// Infos persos de l'utilisateur : affichage et traitement
+// page de contact (mail prérempli)
+
+// Voir tout les travelers associé à un voyage
+// router.get('/travel/id/travelers', travelerController.allTravelers);
+
+//cette route sera protégée par le middleware adminMW
+//Seuls les utilisateurs avec un rôle admin pourront y avoir accès
+// router.get('/admin', adminMW, adminController.admin);
 
 module.exports = router;
