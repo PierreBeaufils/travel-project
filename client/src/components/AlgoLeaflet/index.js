7c0561837a57d4c19a3169d25f5cfa0dddb8907e
wassime-code <wassime.slaiman@gmail.com>
2020-10-26 18:24:22 +0100

    API keys separated from components

--- client/src/components/AlgoLeaflet/index.js
+++ client/src/components/AlgoLeaflet/index.js
@@ -6,12 +6,12 @@ import {
 } from 'react-leaflet';
 import AlgoliaPlaces from 'algolia-places-react';
 import PropTypes from 'prop-types';
+import { appId, apiKey, mapBoxToken } from '../../config'; // importation des clefs API
 import './style.scss';
 
 const AlgoLeaflet = ({
   isMapRequired, isAdressInputRequired, setLocationData, mapPositionToDisplay,
 }) => {
-  const mapBoxToken = 'pk.eyJ1IjoiaXppbWVsbG93IiwiYSI6ImNrZzBvOTh5ZDAweWIyem4xenMyOWY5cmgifQ.t75gYDPDZoUegSq0G6kqgA'; // Token Mapbox api key
   const [currentPosition, setPosition] = useState([mapPositionToDisplay.x, mapPositionToDisplay.y]);
   // useEffect(() => {
   //   console.log('changement de position');
@@ -25,8 +25,8 @@ const AlgoLeaflet = ({
           className="inputalgolia"
           placeholder="Saisissez le lieu"
           options={{
-            appId: 'plP2U9KPBVXF', // Token Algolia Application id
-            apiKey: '031d6d4d8488a26b60b59b7f78c474dc', // Token Algolia API key
+            appId, // Token Algolia Application id
+            apiKey, // Token Algolia API key
             useDeviceLocation: false,
 
           }}
@@ -77,7 +77,6 @@ AlgoLeaflet.propTypes = {
   isAdressInputRequired: PropTypes.bool.isRequired,
   setLocationData: PropTypes.func.isRequired,
   mapPositionToDisplay: PropTypes.object,
-
 };
 
 export default AlgoLeaflet;
