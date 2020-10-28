579b579706e61fab216ecbef4606669e672b8e62
bruno973 <sbad973@gmail.com>
2020-10-28 19:59:29 -0300

    ajout de getUrl pour assembler les URL des fichiers manque getName

--- back/app/services/test.js
+++ back/app/services/test.js
@@ -1,10 +1,12 @@
 const Document = require("../models/Document");
 
 const document1 = new Document({
-    Key: "15/public/Banane peze horizontal.mp4"
+    Key: "15/public/Banane peze horizontal.mp4",
+    url: ".s3.amazonaws.com/"
 });
 
-console.log(document1.getUrl());
+
+console.log(document1.url);
 
 
 
