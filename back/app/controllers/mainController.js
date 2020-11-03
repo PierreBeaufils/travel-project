ed995e8a2293bb9a106385651b583994f36f2473
LeMellow <pierre.beaufils@outlook.fr>
2020-11-03 17:08:47 +0100

    fix showdocuments method and showallinfos method

--- back/app/controllers/mainController.js
+++ back/app/controllers/mainController.js
@@ -2,134 +2,131 @@ const Accommodation = require("../models/Accommodation");
 const Activity = require("../models/Activity");
 const Transport = require("../models/Transport");
 const Travel = require("../models/Travel");
-const Task = require ("../models/Task");
+const Task = require("../models/Task");
 const Traveler = require("../models/Traveler");
 const Document = require("../models/Document");
 const travel_has_traveler = require("../models/Travel_has_travelers");
 const mailApp = require("../services/nodemailer");
 const crypt = require("../services/crypt");
 
-const objectModel = [Accommodation,Activity,Transport,Travel, Task, Traveler, travel_has_traveler];
+const objectModel = [Accommodation, Activity, Transport, Travel, Task, Traveler, travel_has_traveler];
 
 const travelController = {
-    showAllInfos: async (req,res) => {
-        
+    showAllInfos: async (req, res) => {
+
         const travelId = req.params.id;
 
-        let  travelinfos = {};
-        travelinfos = await Travel.findOneTravelComponent(null,travelId);
+        let travelinfos = {};
+        travelinfos = await Travel.findOneTravelComponent(null, travelId);
 
         if (travelinfos) {
             const travelersInTravel = await travel_has_traveler.findTravelersByTravel(travelId);
-            for (traveler of travelersInTravel) {
-                // console.log(req.session.user.id);
-                // console.log(traveler.traveler_id);
-                if (req.session.user.id === traveler.traveler_id) {
-                    travelinfos.prices = await Travel.findPrice(travelId);
-                    travelinfos.traveler = await travel_has_traveler.findTravelersByTravel(travelId);
-                    travelinfos.transport = await Transport.findAllTravelComponent(travelId);
-                    travelinfos.accommodation = await Accommodation.findAllTravelComponent(travelId);
-                    travelinfos.activity = await Activity.findAllTravelComponent(travelId);
-                    travelinfos.task = await Task.findAllTravelComponent(travelId);
-                    travelinfos.documents =  travelController.showDocuments(req,res,travelinfos);
-                    
-                   res.json(travelinfos);
-                } else {
-                   return res.json('Vous n\'avez pas accès à ce voyage');
-                }
+            const travelerAuthorized = travelersInTravel.find((traveler) => traveler.traveler_id === req.session.user.id);
+
+            if (travelerAuthorized) {
+                travelinfos.prices = await Travel.findPrice(travelId);
+                travelinfos.traveler = await travel_has_traveler.findTravelersByTravel(travelId);
+                travelinfos.transport = await Transport.findAllTravelComponent(travelId);
+                travelinfos.accommodation = await Accommodation.findAllTravelComponent(travelId);
+                travelinfos.activity = await Activity.findAllTravelComponent(travelId);
+                travelinfos.task = await Task.findAllTravelComponent(travelId);
+                travelinfos.documents = await travelController.showDocuments(req, res, travelinfos);
+
+                res.status(200).json(travelinfos);
+            } else {
+                return res.status(404).json('Vous n\'avez pas accès à ce voyage');
             }
-        } else {
+        }
+        else {
             res.status(404).json('ce voyage n\'existe pas');
         };
     },
-    showTravels: async (req, res) =>{
+    showTravels: async (req, res) => {
         let travelInfos = {};
         travelInfos = await Travel.findAllTravelComponent();
 
-        if (travelInfos) { 
+        if (travelInfos) {
             res.json(travelInfos);
         } else {
-            res.status(404).json ('Il n\'existe pas de voyage');
+            res.status(404).json('Il n\'existe pas de voyage');
         };
     },
-    createTravel: async (req,res) => {
+    createTravel: async (req, res) => {
         const newTravel = new Travel(req.body);
         const savedTravel = await newTravel.saveAllTravelComponent();
 
         const travel_id = savedTravel.id;
         const traveler_id = req.body.owner;
 
-        const newTravelerToTravel = new travel_has_traveler({travel_id, traveler_id});
+        const newTravelerToTravel = new travel_has_traveler({ travel_id, traveler_id });
         newTravelerToTravel.saveTravelerIntoTravel();
 
-        res.json('Voyage créé'); 
+        res.json('Voyage créé');
         // Theme / destination / date départ / date de fin
     },
-    editTravel: async (req,res) => {
-        const travelToEdit = await Travel.findOneTravelComponent(null,req.params.id);
+    editTravel: async (req, res) => {
+        const travelToEdit = await Travel.findOneTravelComponent(null, req.params.id);
         const travelEdited = await new Travel(travelToEdit);
         travelEdited.update(req.body);
         await travelEdited.saveAllTravelComponent();
         res.json('voyage mis à jour')
     },
-    deleteTravel: async (req,res) => {
-        const travelToFind = await Travel.findOneTravelComponent(null,req.params.id);
+    deleteTravel: async (req, res) => {
+        const travelToFind = await Travel.findOneTravelComponent(null, req.params.id);
         if (travelToFind) {
             const travelToDelete = new Travel(travelToFind);
             await travelToDelete.delete();
-            res.json("suppression effectuée");    
+            res.json("suppression effectuée");
         } else {
             res.status(404).json('cette entité n\'existe pas')
         }
     },
-    showUserTravels : async (req,res) =>{
+    showUserTravels: async (req, res) => {
         const travelerId = req.params.id;
 
         const userTravels = await Travel.findAllTravels(travelerId);
-        
+
         if (userTravels) {
             res.json(userTravels);
         } else {
             res.status(404).json('ce voyageur n\'appartient à aucun voyage')
         }
     },
-    showTravelers: async (req, res) =>{
-        const travelId = req.params.id ;
+    showTravelers: async (req, res) => {
+        const travelId = req.params.id;
         const travelersInTravel = await travel_has_traveler.findTravelersByTravel(travelId);
         // console.log(travelersInTravel);
-        
-        if (travelersInTravel.length > 0) { 
+
+        if (travelersInTravel.length > 0) {
             res.json(travelersInTravel);
         } else {
-            res.status(404).json ('Ce voyage ne comporte pas encore de voyageurs');
+            res.status(404).json('Ce voyage ne comporte pas encore de voyageurs');
         };
     },
-    sendTravelInvitation: async (req,res) => {
-        const travelersToAssoc = req.body ;
-        
-        
+    sendTravelInvitation: async (req, res) => {
+        const travelersToAssoc = req.body;
 
         for (let travelerMail in travelersToAssoc) {
-        let token = crypt.getTokenTravel(req.params.id,travelersToAssoc[travelerMail]);
-        console.log(token);
+            let token = crypt.getTokenTravel(req.params.id, travelersToAssoc[travelerMail]);
+            console.log(token);
 
-        let text = `Bonjour, 
+            let text = `Bonjour, 
         Un membre de "Globe Trotter" à décidé d'organiser un voyage et souhaite vous inviter à en faire parti,
         pour accepter son invitation cliquer simplement ici : http://localhost:8080/rejoindre/?token=${token}
         `;
-        let html = `<h1> Bonjour,</h1>
+            let html = `<h1> Bonjour,</h1>
         <p> Un membre de "Globe Trotter" à décidé d'organiser un voyage et souhaite vous inviter à en faire parti,
         pour accepter son invitation cliquer simplement ici : <a href="http://localhost:8080/rejoindre/?token=${token}">Rejoindre un voyage</a>
         `;
-            let message = mailApp.messageConstructor(travelersToAssoc[travelerMail],token);
-            message.text = text ;
-            message.html = html ;
-           mailApp.transporter.sendMail(message);
+            let message = mailApp.messageConstructor(travelersToAssoc[travelerMail], token);
+            message.text = text;
+            message.html = html;
+            mailApp.transporter.sendMail(message);
         }
         res.status(200).json("Ces voyageurs ont bien été invités");
     },
     addTravelers: async (req, res) => {
-        
+
         let decryptedToken = crypt.decryptText(req.query.token);
         decryptedToken = decryptedToken.split("-");
         console.log(decryptedToken);
@@ -138,26 +135,26 @@ const travelController = {
         if (foundTraveler) {
             // Que doit il se passer si l'utilisateur est déja un traveler ?
             new travel_has_traveler(foundTraveler);
-        
-    
-        
-        
-       
-        const newTraveler = new travel_has_traveler({
-            travel_id: decryptedToken[0],
-            traveler_id: foundTraveler.id
-        });
-        const addedTraveler = await newTraveler.saveTravelerIntoTravel();
-        res.json("Vous avez bien été ajouté à ce voyage");
-    } else {
-       res.status(404).json("Il semble que vous ne soyez pas encore inscrit sur Globe Trotter, veuillez vous inscrire sur notre plateforme en suivant ce lien : http://localhost:8080/inscription")
-    }
-        
-        
-        
-        
+
+
+
+
+
+            const newTraveler = new travel_has_traveler({
+                travel_id: decryptedToken[0],
+                traveler_id: foundTraveler.id
+            });
+            const addedTraveler = await newTraveler.saveTravelerIntoTravel();
+            res.json("Vous avez bien été ajouté à ce voyage");
+        } else {
+            res.status(404).json("Il semble que vous ne soyez pas encore inscrit sur Globe Trotter, veuillez vous inscrire sur notre plateforme en suivant ce lien : http://localhost:8080/inscription")
+        }
+
+
+
+
     },
-    deleteTravelerFromTravel : async (req, res) => {
+    deleteTravelerFromTravel: async (req, res) => {
         const travelerToFind = await travel_has_traveler.findOneTravelerByTravel(req.params.id, req.params.travelerId);
         // console.log(travelerToFind);
         if (travelerToFind) {
@@ -169,11 +166,11 @@ const travelController = {
             res.status(404).json('ce voyageur n\'est pas inscrit sur ce voyage')
         }
     },
-    showEntity: async (req,res) => {
+    showEntity: async (req, res) => {
         let entity = req.params.entity;
         let entityToUse;
 
-        for (let i = 0 ; i < objectModel.length ; i++) {
+        for (let i = 0; i < objectModel.length; i++) {
             if (entity === objectModel[i].tableName) {
                 entityToUse = objectModel[i];
             }
@@ -181,36 +178,34 @@ const travelController = {
 
         const entityToFind = await entityToUse.findAllTravelComponent(req.params.id);
         if (entityToFind.length > 0) {
-            res.json (entityToFind);
+            res.json(entityToFind);
         } else {
-            res.status(404).json (`Il y a 0 ${entityToUse.tableName} dans ce voyage`);
+            res.status(404).json(`Il y a 0 ${entityToUse.tableName} dans ce voyage`);
         }
     },
-    showDocuments : async (req,res,travelinfos) => {
-        // Je recupere l'id du voyage
+    showDocuments: async (req, res, travelinfos) => {
         const prefix = req.params.id + "/public/";
         const documents = await Document.getAllPublic(prefix);
-        // console.log(documents.Contents);
-       if (documents.Contents === undefined) {
-           travelinfos.documents = "Pas de document pour ce voyage";
-       } 
-       else {
-        const documentsToShow = [];
-        for (let object of documents.Contents) {
-            if (object.Size != 0) {
-            object.travel_id = req.params.id;
-            let documentToPush = await new Document(object);
 
-            delete documentToPush.ETag ;
-            delete documentToPush.StorageClass;
-            documentsToShow.push(documentToPush);
+        let documentsList = [];
+        if (documents.Contents === undefined) {
+            return documentsList;
         }
+        else {
+            for (let object of documents.Contents) {
+                if (object.Size != 0) {
+                    object.travel_id = req.params.id;
+                    let documentToPush = await new Document(object);
+
+                    delete documentToPush.ETag;
+                    delete documentToPush.StorageClass;
+                    documentsList.push(documentToPush);
+                }
+            }
+            return documentsList;
         }
-        travelinfos.documents = documentsToShow;
-        // res.json(documentsToShow);
-    }
     },
-    createEntity: async (req,res) => {
+    createEntity: async (req, res) => {
         let entity = req.params.entity;
         let entityToUse;
 
@@ -231,7 +226,7 @@ const travelController = {
         }
 
         else {
-            for (let i = 0 ; i < objectModel.length ; i++) {
+            for (let i = 0; i < objectModel.length; i++) {
                 if (entity === objectModel[i].tableName) {
                     entityToUse = objectModel[i];
                 }
@@ -242,24 +237,24 @@ const travelController = {
 
             await newEntity.saveAllTravelComponent();
             res.json('Ajout effectué');
-        }        
-    },   
-    editEntity: async (req,res) => {
+        }
+    },
+    editEntity: async (req, res) => {
         let entity = req.params.entity;
-        let entityToUse ;
+        let entityToUse;
 
         if (entity === 'document') {
             res.json("Un document ne peux être édité, veuillez le supprimer et ajouter votre nouveau document")
         }
         else {
-            for (let i = 0 ; i < objectModel.length ; i++) {
+            for (let i = 0; i < objectModel.length; i++) {
                 // console.log('objectModel[i].tableName', objectModel[i].tableName);
                 if (entity === objectModel[i].tableName) {
                     entityToUse = objectModel[i];
                 }
             };
 
-            const entityToEdit = await entityToUse.findOneTravelComponent(req.params.id,req.params.entityId);
+            const entityToEdit = await entityToUse.findOneTravelComponent(req.params.id, req.params.entityId);
 
             if (entityToEdit) {
                 const editedEntity = await new entityToUse(entityToEdit);
@@ -271,24 +266,24 @@ const travelController = {
             }
         }
     },
-    deleteEntity: async (req,res) => {
+    deleteEntity: async (req, res) => {
         let entity = req.params.entity;
-        let entityToUse ;
+        let entityToUse;
 
         if (entity === 'document') {
-            const documentToDelete = req.body ;
+            const documentToDelete = req.body;
             const deletedDocument = new Document(documentToDelete);
             const deleteConfirmation = await deletedDocument.deleteFile();
             res.json(deleteConfirmation);
         }
-        
-        for (let i = 0 ; i < objectModel.length ; i++) {
+
+        for (let i = 0; i < objectModel.length; i++) {
             if (entity === objectModel[i].tableName) {
                 entityToUse = objectModel[i];
             }
         };
-        
-        const entityToFind = await entityToUse.findOneTravelComponent(req.params.id,req.params.entityId);
+
+        const entityToFind = await entityToUse.findOneTravelComponent(req.params.id, req.params.entityId);
         if (entityToFind) {
             const entityToDelete = new entityToUse(entityToFind);
             await entityToDelete.delete();
@@ -296,7 +291,7 @@ const travelController = {
         } else {
             res.status(404).json("cette entité n'existe pas dans ce voyage");
         }
-    }    
+    }
 };
 
-module.exports = travelController ;
+module.exports = travelController;
