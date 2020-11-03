aad32a04527404ed76c42491c4ef728259c9cade
LeMellow <pierre.beaufils@outlook.fr>
2020-11-03 14:19:30 +0100

    css

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -8,25 +8,23 @@ const Contact = ({ user }) => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="page-container">
-      <div className="contact-container">
-        <div className="main-form">
-          <h2>Nous contacter</h2>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name">Nom
-              <input name="name" ref={register({ required: true })} type="text" defaultValue={user ? `${user.first_name} ${user.last_name}` : ''} />
-            </label>
-            {errors.name && <span className="warning-text">Veuillez saisir votre nom</span>}
-            <label htmlFor="email">Email
-              <input name="email" ref={register({ required: true })} type="email" defaultValue={user ? user.email : ''} />
-            </label>
-            {errors.email && <span className="warning-text">Veuillez saisir votre email</span>}
-            <label htmlFor="message">Message
-              <textarea name="message" ref={register({ required: true })} minLength="50" rows="10" />
-            </label>
-            <input type="submit" className="submit-button" value="Envoyer" />
-          </form>
-        </div>
+    <div className="contact-container">
+      <div className="main-form">
+        <h2>Nous contacter</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="name">Nom
+            <input name="name" ref={register({ required: true })} type="text" defaultValue={user ? `${user.first_name} ${user.last_name}` : ''} />
+          </label>
+          {errors.name && <span className="warning-text">Veuillez saisir votre nom</span>}
+          <label htmlFor="email">Email
+            <input name="email" ref={register({ required: true })} type="email" defaultValue={user ? user.email : ''} />
+          </label>
+          {errors.email && <span className="warning-text">Veuillez saisir votre email</span>}
+          <label htmlFor="message">Message
+            <textarea name="message" ref={register({ required: true })} minLength="50" rows="10" />
+          </label>
+          <input type="submit" className="submit-button" value="Envoyer" />
+        </form>
       </div>
     </div>
   );
