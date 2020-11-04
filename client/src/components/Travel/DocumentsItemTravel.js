eeacbaf74373534d6bcea2a1be755dccd004aa79
LeMellow <pierre.beaufils@outlook.fr>
2020-11-04 11:22:37 +0100

    upload docs ok

--- client/src/components/Travel/DocumentsItemTravel.js
+++ client/src/components/Travel/DocumentsItemTravel.js
@@ -6,24 +6,29 @@ import {
 
 import './travel.scss';
 
-const DocumentItem = ({ name, LastModified, url, Bucket, Key, Size }) => {
+const DocumentItem = ({
+  travel_id, Bucket, Key, LastModified, Size, url, name,
+}) => {
   const timestampInDate = new Date(LastModified); // creation d'une instance date
   const DateInLocalString = timestampInDate.toLocaleString(); // date convertie en string
-  
 
   return (
     <li className="document_item">
       <a href={url}><Download />  {name}</a>
       <p className="document__item_details">Mis à jour le: {DateInLocalString}</p>
-      <p className="document__item_details">Taille:  {Size/8000} Ko</p>
+      <p className="document__item_details">Taille:  {Size / 8000} Ko</p>
     </li>
   );
 };
 
 DocumentItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  travel_id: PropTypes.string.isRequired,
+  Bucket: PropTypes.string.isRequired,
+  Key: PropTypes.string.isRequired,
+  LastModified: PropTypes.string.isRequired,
+  Size: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default DocumentItem;
