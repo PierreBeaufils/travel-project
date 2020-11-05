5fd571a2a4495a61c0447258c0e873839ca1f742
LeMellow <pierre.beaufils@outlook.fr>
2020-11-05 15:11:27 +0100

    ip cors

--- back/app/server.js
+++ back/app/server.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5555;
 const router = require('./router');
 
 app.use(cors({
-    origin: ['http://globe-trotter.surge.sh/', 'http://localhost:8080'],
+    origin: ['http://138.197.235.123', 'http://localhost:8080'],
     credentials: true,
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token',
