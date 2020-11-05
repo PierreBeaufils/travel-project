684013ee504aaae09dee3d8483199d6c1d407752
LeMellow <pierre.beaufils@outlook.fr>
2020-11-05 10:49:03 +0100

    css cards team

--- client/src/components/Team/index.js
+++ client/src/components/Team/index.js
@@ -12,9 +12,35 @@ const Team = () => (
           <User color="#2B7AFD" size={100} />
         </div>
         <div className="teamcard-content">
+          <p className="teamcard-content-name">Bruno Sosin</p>
+          <p className="teamcard-content-role">Product Owner/Git Master</p>
+          <p className="teamcard-content-description">Maire de Cayenne</p>
+          <div className="teamcard-content-social">
+          </div>
+        </div>
+      </div>
+
+      <div className="teamcard">
+        <div className="teamcard-picture">
+          <User color="#2B7AFD" size={100} />
+        </div>
+        <div className="teamcard-content">
+          <p className="teamcard-content-name">Salim Bennis</p>
+          <p className="teamcard-content-role">Lead Dev Back</p>
+          <p className="teamcard-content-description">Petit ange parti trop tôt</p>
+          <div className="teamcard-content-social">
+          </div>
+        </div>
+      </div>
+
+      <div className="teamcard">
+        <div className="teamcard-picture">
+          <User color="#2B7AFD" size={100} />
+        </div>
+        <div className="teamcard-content">
           <p className="teamcard-content-name">Pierre Beaufils</p>
           <p className="teamcard-content-role">Lead Dev Front</p>
-          <p className="teamcard-content-description">blabldsq asdqd  dsqdsqddsq sqd</p>
+          <p className="teamcard-content-description">L'important c'est les 3 points</p>
           <div className="teamcard-content-social">
             <a href="https://www.linkedin.com/in/pierre-beaufils/">
               <Linkedin />
@@ -28,19 +54,14 @@ const Team = () => (
           <User color="#2B7AFD" size={100} />
         </div>
         <div className="teamcard-content">
-          <p className="teamcard-content-name">Pierre Beaufils</p>
-          <p className="teamcard-content-role">Lead Dev Front</p>
-          <p className="teamcard-content-description">blabldsq asdqd  dsqdsqddsq sqd</p>
+          <p className="teamcard-content-name">Wassime Slaiman</p>
+          <p className="teamcard-content-role">Scrum master</p>
+          <p className="teamcard-content-description">L'important c'est l'essentiel, et souvent l'essentiel c'est le plus important</p>
           <div className="teamcard-content-social">
-            <a href="https://www.linkedin.com/in/pierre-beaufils/">
-              <Linkedin />
-            </a>
           </div>
         </div>
       </div>
-
     </div>
-    <h3>Conditions d'utilisation</h3>
   </div>
 );
 
